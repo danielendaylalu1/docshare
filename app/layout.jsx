d3614ab8e2aa057b7974f427ed8ddf8389d686fe
daniel endaylalu <endaylaludaniel@gmail.com>
2024-05-19 10:48:55 +0300

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={` bg-white px-6 py-4 min-h-[100vh]`}>{children}</body>
+      <body className={` bg-white px-2 py-4 min-h-[100vh]`}>{children}</body>
     </html>
   );
 }
